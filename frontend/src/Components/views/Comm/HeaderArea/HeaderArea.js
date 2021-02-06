948855785f5712fc7966d4eb3472df9f68031d24
omk0928 <omk0928@gmail.com>
2021-02-06 19:57:09 +0900

    mysql 재설정

--- frontend/src/Components/views/Comm/HeaderArea/HeaderArea.js
+++ frontend/src/Components/views/Comm/HeaderArea/HeaderArea.js
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Layout, Divider } from "antd";
 
 export default function HeaderArea() {
   const { Header } = Layout;
@@ -6,9 +6,21 @@ export default function HeaderArea() {
   return (
     <Header
       className="site-layout-background"
-      style={{ padding: 0, textAlign: "center" }}
     >
-      <h1>한국ㅈ리텍대학교 ㅈ성캠퍼스 학사관리시스템</h1>
+      <div>
+        <h6
+          style={{ padding: 0, textAlign: "right" }}>
+          로그인
+        <Divider type="vertical" />
+          <>공지사항</>
+          <Divider type="vertical" />
+          <>문의</>
+        </h6>
+        <h2
+          style={{ padding: 0, textAlign: "center" }}>
+          Kimohchoi 대학
+      </h2>
+      </div>
     </Header>
   );
 }
